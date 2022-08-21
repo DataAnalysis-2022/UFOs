2483bc90815b6620cafb19d6f22e27d514174af2
DataAnalysis-2022 <106888594+DataAnalysis-2022@users.noreply.github.com>
2022-08-21 12:15:15 -0400

    New Files

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,8 @@
 // from data.js
 const tableData = data;
 
+console.log('working')
+
 // get table references
 var tbody = d3.select("tbody");
 
@@ -33,11 +35,15 @@ function updateFilters() {
     var inputText = d3.select(this);
 
     // 4b. Save the value that was changed as a variable.
-    var filterValue = inputText.text.property("value");
-    //var filterValue = d3.select.event.target.value;
+    var filterValue = inputText.property("value");
+    
 
     // 4c. Save the id of the filter that was changed as a variable.
-    var filterId = inputText.id.property("value");
+    
+    var filterId = inputText.attr("id");
+   
+    console.log(filterId);
+    console.log(filterValue);
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
@@ -60,17 +66,22 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    filters.forEach((item)=>{
+    const keys = Object.keys(filters);
+    const values = Object.values(filters);
 
-          filteredData = filteredData.filter(row => {row.key === item.key; row.value===item.value});
-    });
+    console.log(keys, values);
+
+    for (let i =0; i<keys.length; i++){
+
+          filteredData = filteredData.filter((row) => {return row[keys[i]] === values[i]});
+    };
 
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
   }
   
   // 2. Attach an event to listen for changes to each filter
-  d3.selectAll("#input").on("change", updateFilters);
+  d3.selectAll("input").on("change", updateFilters);
   
   // Build the table when the page loads
   buildTable(tableData);
